6983b471419d4fac1223e3ee8d103b81b3ffa557
MaThanMiThun1999 <mathanmithun8838@gmail.com>
2024-08-10 16:04:00 +0530

    HeadlessMode

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const GitHubRefresher = async (URL) => {
         "--disable-gpu",
         "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36",
       ],
-      headless: false,
+      headless: true,
     });
 
     const page = await browser.newPage();
